dac9be08d5deae2713e0690081e69fd078234a4e
Joshua Barnett <joshua.barnett@familysearch.org>
2023-01-30 11:51:57 -0700

    got Dice setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import './App.css';
+import Die from "./components/die"
 
 function App() {
+  function randomNumber(){
+    return Math.floor(Math.random() * 6)+1;
+
+  }
   return (
-    <div className="gameboard">
-      Hi gameboard!
+    <div className="game">
+      <div className="dice-container">
+        <Die value={randomNumber()}/>
+        <Die value={randomNumber()}/>
+        <Die value={randomNumber()}/>
+        <Die value={randomNumber()}/>
+        <Die value={randomNumber()}/>
+        <Die value={randomNumber()}/>
+        <Die value={randomNumber()}/>
+        <Die value={randomNumber()}/>
+        <Die value={randomNumber()}/>
+        <Die value={randomNumber()}/>
+      </div>
     </div>
   );
 }
