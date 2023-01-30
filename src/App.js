1b8c944197ff5a6c3a756ec3f6b3bf60fabb0399
Joshua Barnett <joshua.barnett@familysearch.org>
2023-01-30 12:09:20 -0700

    roll button set up

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
 import './App.css';
 import Die from "./components/die"
+import {useState} from "react";
 
 function App() {
-  function randomNumber(){
-    return Math.floor(Math.random() * 6)+1;
+  const[dice, setDice] = useState(allNewDice)
 
+  function allNewDice() {
+    let diceArray = []
+    for (let x = 0; x < 10; x++) {
+      diceArray.push(<Die value={Math.floor(Math.random() * 6) + 1}/>)
+    }
+    return diceArray;
   }
+
+  function rollButtonClicked () {
+    setDice(allNewDice)
+  }
+
   return (
     <div className="game">
       <div className="dice-container">
-        <Die value={randomNumber()}/>
-        <Die value={randomNumber()}/>
-        <Die value={randomNumber()}/>
-        <Die value={randomNumber()}/>
-        <Die value={randomNumber()}/>
-        <Die value={randomNumber()}/>
-        <Die value={randomNumber()}/>
-        <Die value={randomNumber()}/>
-        <Die value={randomNumber()}/>
-        <Die value={randomNumber()}/>
+        {dice}
       </div>
+      <button className="roll-dice" onClick={rollButtonClicked}>Roll</button>
     </div>
   );
 }
