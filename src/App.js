8136f9a187eabe282aa5aa562e26194ca33a911f
Joshua Barnett <joshua.barnett@familysearch.org>
2023-01-30 14:36:19 -0700

    winning the game start

--- src/App.js
+++ src/App.js
@@ -1,40 +1,74 @@
 import './App.css';
 import Die from "./components/die"
-import {useState} from "react";
+import {useState, useEffect} from "react";
 import {nanoid} from "nanoid"
+
+
 
 function App() {
   const [dice, setDice] = useState(allNewDice)
+  const [tenzie, setTenzie] = useState(false)
+
+
+  useEffect(()=>{
+    let isWon = dice.filter(dice => dice.isHeld === true)
+    if(isWon.length === 10){
+      setTenzie(true)
+      console.log("You Won!!!!")
+    }
+
+  }, [dice])
 
   function genRandNum() {
     return Math.floor(Math.random() * 6) + 1;
   }
 
+  function genNewDie() {
+    return {id: nanoid(), value: genRandNum(), isHeld: false}
+  }
+
+  function resetGame(){
+    setDice(allNewDice)
+    setTenzie(false)
+  }
+
   function allNewDice() {
     let diceArray = []
     for (let x = 0; x < 10; x++) {
-      diceArray.push({id: nanoid(), value: genRandNum(), isHeld: false})
+      diceArray.push(genNewDie())
     }
 
     return diceArray;
   }
 
   function holdDice(id) {
-    console.log(id);
+    setDice(oldDice => oldDice.map(die => {
+              return die.id === id ? {...die, isHeld: !die.isHeld} : die
+            })
+    )
   }
 
   function rollButtonClicked() {
-    setDice(allNewDice)
+    setDice(oldDice => oldDice.map(die => {
+      if (die.isHeld) {
+        return die;
+      }
+      else {
+        return genNewDie();
+      }
+    }))
   }
 
   let diceElements = dice.map(die => <Die key={die.id} value={die.value} isHeld={die.isHeld} hold={() => holdDice(die.id)}/>)
 
   return (
     <div className="game">
+      <h1 className="title">Tenzies</h1>
+      <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
       <div className="dice-container">
         {diceElements}
       </div>
-      <button className="roll-dice" onClick={rollButtonClicked}>Roll</button>
+      <button className="roll-dice" onClick={tenzie ? resetGame : rollButtonClicked}>{tenzie ? "Restart" : "Roll"}</button>
     </div>
   );
 }
