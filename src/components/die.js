22fcf5da51a2cf3bca2791167bea3f3e21338011
Joshua Barnett <joshua.barnett@familysearch.org>
2023-01-30 22:27:53 -0700

    made dice look like real dice.

--- src/components/die.js
+++ src/components/die.js
@@ -1,11 +1,19 @@
-export default function Die(props){
+export default function Die(props) {
   const styles = {
-    backgroundColor: props.isHeld ? "#59E391" :"white"
+    backgroundColor: props.isHeld ? "#59E391" : "white"
   }
 
-  return(
+  function numStyle(value) {
+    let pipArray = []
+    for (let i = 0; i < value; i++) {
+      pipArray.push(<span className="pip"/>)
+    }
+    return pipArray
+  }
+
+  return (
     <div className={`die-face`} style={styles} onClick={props.hold}>
-      <h2 className="die-num">{props.value}</h2>
+      {numStyle(props.value)}
     </div>
 
   )
